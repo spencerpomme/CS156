15ec985e1592d0ab70984109660a70ce7a778fb3
スペンサー <spencerpomme@live.com>
2018-01-26 16:40:56 +0800

    Make it cleaner

--- perceptron.py
+++ perceptron.py
@@ -12,35 +12,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as rd
 
-xrange = [-1, 1]
-yrange = [-1, 1]
-
-xs = []
-ys = []
-
-# Generate a randomly decided line (in vector form)
-# vecline[0] is the slope, vecline[1] is the interception
-p1 = [rd.uniform(-1, 1), rd.uniform(-1, 1)]
-p2 = [rd.uniform(-1, 1), rd.uniform(-1, 1)]
-plt.xlim(*xrange)
-plt.ylim(*yrange)
-# plot the true line
-plt.plot([p1[0], p2[0]], [p1[1], p2[1]], 'r-', linewidth=2)
-
-
-k = (p2[1] - p1[1])/(p2[0]-p1[0])
-b = p1[1] - k * p1[0]
-
-# tranform y = kx + b to Ax + By + C = 0
-A = -k
-B = 1
-C = -b
-w_truth = [C,A,B]
-
 # Using the randomly generated parameter to define the real target function
 def target(x, y):
-    return 1 if np.dot(w_truth, np.transpose([1,x,y])) > 0 else -1
-    
+    # print(np.dot(w_truth, [1, x, y]))
+    if np.dot(w_truth, [1, x, y]) > 0:
+        return 1
+    else:
+        return -1
+    # eturn 1 if np.dot(w_truth, np.transpose([1,x,y])) > 0 else -1
 
 # Generate n random points according to the target function
 def gen_pts(n: int):
@@ -49,48 +28,88 @@ def gen_pts(n: int):
         x = rd.uniform(-1, 1)
         y = rd.uniform(-1, 1)
         yield [x, y, target(x, y)]
-        
-# plot the points
-for i in gen_pts(100):
-    # print(i)
-    xs.append([i[0], i[1]])
-    ys.append(i[2])
-    plt.plot(i[0], i[1], *['g+' if i[2]>0 else 'b_'])
 
 
-print(xs, ys)
 # definition of the perceptron learning algorithm
-def perceptron(Input, label, w):
+def perceptron(Input, label, w, miss):
     """
     Input: list of input value, [x1, x2, x3,...,xn]
     label: + or -
     w: initial weights for training
     """
     # print('w->', w)
+    global track
     assert len(Input) == len(label)
     for i in range(len(Input)):
         x = [1]
         x.extend(Input[i])
-        
+        # print('np.dot(w, x):', np.dot(w, x), 'label[i]: ', label[i])
         if np.dot(w, x) * label[i] < 0:
+            miss[i] = 1
             w = list(np.add(w, np.multiply(x, label[i])))
-    return w
+            track += 1
+        else:
+            miss[i] = 0
+    return w, miss
 
 # this train method doesn't work very good
-def train(p, Input, label, w, iteration):
-    if iteration == 0:
-        return w
-    else:
-        # print('remaining iteration: ', iteration)
-        return train(p, Input, label, p(Input, label, w), iteration-1)
+def train(p, Input, label, w):
+    miss = [1] * N
+    print('miss before iter:', miss)
+    print('inital weight:', w)
+    iteration = 1
+    while sum(miss) >= N * 0.01:
+        print('interation {}'.format(iteration))
+        new_w, miss = p(Input, label, w, miss)
+        print('new_w -> ', new_w)
+        w = new_w
+        iteration += 1
+        print('miss at iteration {}'.format(iteration), miss)
+    miss = [1] * N
+    return w
 
-w_hypoth = train(perceptron, xs, ys, [-4,2,1], 900)
 
+if __name__ == "__main__":
+    # maigc number don't delete
+    track = 0
+    # Number of points
+    N = 100
+    # Set line parameters: x + y + 1= 0
+    A = 1
+    B = 1
+    C = 1
+    w_truth = [C, A, B]
+    
+    # The sample set {xs -> ys}
+    xs = []
+    ys = []
+    # control points of line x+y+1=0:
+    p1 = [-1, 0]
+    p2 = [0, -1]
+    # plot the points
+    for i in gen_pts(N):
+        # print(i)
+        xs.append([i[0], i[1]])
+        ys.append(i[2])
+        plt.plot(i[0], i[1], *['g+' if i[2]>0 else 'b_'])
+    
+    plt.plot([-1, 0], [0, -1], 'r--', linewidth=3)
+    
+    w_hypoth = train(perceptron,
+                     xs,
+                     ys,
+                     [rd.randint(-10,10),
+                      rd.randint(-10,10),
+                      rd.randint(-10,10)
+                      ]
+                     )
 
-print('w_truth -> ', w_truth)
-print('w_hypoth -> ', w_hypoth)
-
-# plot the true line
-plt.plot([0, -w_hypoth[0]/w_hypoth[2]], [-w_hypoth[0]/w_hypoth[1], 0], 'yo-', linewidth=2)
-
-plt.show()
+    print('w_truth -> ', w_truth)
+    print('w_hypoth -> ', w_hypoth)
+    print('track(or the actual iteration): ', track)
+    
+    # plot the true line
+    plt.plot([0, -w_hypoth[0]/w_hypoth[2]], [-w_hypoth[0]/w_hypoth[1], 0],
+             'y-', linewidth=2, label="hypothesis")
+    
+    plt.show()
